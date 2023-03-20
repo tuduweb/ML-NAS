77625477fc4e6b49a905581b01bdd91a75436875
tuduweb <tuduweb@qq.com>
2023-03-20 22:02:50 +0800

    抽离出get_files方法

---
+++ experiment/nas-evocnn-autodeploy/basicTools/fileTool.py
@@ -0,0 +1,24 @@
+import os
+
+def get_file(file_path: str, suffix: str, res_file_path: list, keyword = None) -> list:
+    """获取路径下的指定文件类型后缀的文件
+
+    Args:
+        file_path: 文件夹的路径
+        suffix: 要提取的文件类型的后缀
+        res_file_path: 保存返回结果的列表
+
+    Returns: 文件路径
+
+    """
+
+    for file in os.listdir(file_path):
+
+        if os.path.isdir(os.path.join(file_path, file)):
+            get_file(os.path.join(file_path, file), suffix, res_file_path)
+        else:
+            if not keyword or (keyword and keyword in file):
+                res_file_path.append(os.path.join(file_path, file))
+
+    # endswith：表示以suffix结尾。可根据需要自行修改；如：startswith：表示以suffix开头，__contains__：包含suffix字符串
+    return res_file_path if suffix == '' or suffix is None else list(filter(lambda x: x.endswith(suffix), res_file_path))
